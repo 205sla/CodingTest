5c63682aba39bb2c6510c40adf3e6d5b327afe93
YOUNGHO LEE <61982040+205sla@users.noreply.github.com>
2024-11-23 18:37:01 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//205.kr
+int n, m;
+int arr[10];
+vector<int> L;
+bool isused[10];
+void func1(int cur, int 최대) {
+	if (cur == m) {
+		for (int i = 0; i < m;i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 최대; i < n;i++) {
+		arr[cur] = L[i];
+		func1(cur + 1, i);
+
+
+
+	}
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	cin >> n >> m;
+	for (int i = 0; i < n;i++) {
+		int temp;
+		cin >> temp;
+		L.push_back(temp);
+	}
+
+	sort(L.begin(), L.end());
+	func1(0,0);
+}
